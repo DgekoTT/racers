7ea22edb0e5c0fe0b4ee828b290e2250c0601328
Dgeko <dfhdfh13@gmail.com>
2022-11-29 19:44:59 +0300

    подключился к мгДБ

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 3000;
 
 const app = express();
 
-app.listen(PORT, () => console.log('Server work on PORT' + PORT));
+const db_url = 'mongodb+srv://Dgeko:frGGT14@cluster0.lbrizgf.mongodb.net/?retryWrites=true&w=majority'
+
+async function startApp() {
+    try {
+        await mongoose.connect(db_url)
+        app.listen(PORT, () => console.log('server started on PORT ' + PORT));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startApp();
