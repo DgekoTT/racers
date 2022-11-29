6bc39c1432e2ce83888f05b6f4218a2bcaefdd76
Dgeko <dfhdfh13@gmail.com>
2022-11-29 22:24:33 +0300

    начал разбираться с фронтом, установил ejs, начал исправление base.html страницы

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require("mongoose");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
 
 const app = express();
 
 const db_url = 'mongodb+srv://Dgeko:frGGT14@cluster0.lbrizgf.mongodb.net/?retryWrites=true&w=majority'
+
+const createPath = (page) => path.resolve(__dirname, './views', `${page}.html`);
 
 async function startApp() {
     try {
@@ -17,3 +22,8 @@ async function startApp() {
 }
 
 startApp();
+
+app.get('/', (req, res) => { // выдает главную страницу
+    const title = 'Home';
+    res.sendFile(createPath('base'), {title});
+});
